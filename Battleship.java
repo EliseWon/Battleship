1a3cc99f90fd60c2c47a7ad97ea763bede051312
dmc-account <>
2020-01-12 16:28:23 -0800

    Stuff

--- Battleship.java
+++ Battleship.java
@@ -6,9 +6,13 @@ public class Battleship {
     static int y;
     static int x2;
     static int y2;
-    static int points;
+    static int points = 0;
     static String randomVsManual;
     static boolean xOrY;
+    static int bye1;
+    static int bye2;
+    static int pearls;
+    static String crystalBoba;
 
     public static void main(String args[]) {
         Boba ship1 = new Boba(6, 2, 6, 5, 0);
@@ -17,15 +21,17 @@ public class Battleship {
         System.out.println("Would you like to place your ships randomly or manually?");
         randomVsManual = Hello.nextLine();
 
-        if (randomVsManual == "Randomly") {
-            Boba.randomizeShips();
-        } else if (randomVsManual == "Manually") {
-            Boba.manuallyPlaceShips();
+        if (randomVsManual.equals("Randomly")) {
+            ship1.randomizeShips();
+            ship2.randomizeShips();
+        } else if (randomVsManual.equals("Manually")) {
+            ship1.manuallyPlaceShips();
+            ship2.manuallyPlaceShips();
         } else {
             System.out.println("Invalid response.");
         }
 
-        while (points <= 3) {
+        while (points <= 17) {
             ship1.fire();
             ship2.fire();
         }
--- Boba.java
+++ Boba.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 import java.util.*;
 import java.util.Arrays;
+import java.lang.Object;
+import java.lang.reflect.Array;
 
 public class Boba extends Battleship {
 
@@ -16,18 +18,27 @@ public class Boba extends Battleship {
         Random randomizer = new Random();
 
         int[] milkTea = {2, 3, 3, 4, 5};
+        int i = 0;
 
-        for (Integer i : milkTea) {
+        for (i = 0; i < 5; i++) {
             x = randomizer.nextInt(10);
             y = randomizer.nextInt(10);
-            xOrY = randomizer.nextBoolean();
-            int counter = 0;
+
+            pearls = randomizer.nextInt(10);
+            crystalBoba = String.valueOf(pearls / 2);
+            try {
+                Integer.valueOf(crystalBoba);
+                xOrY = true;
+            } catch (NumberFormatException e) {
+                xOrY = false;
+            }
 
             if (xOrY = true) {
                 x2 = x;
-                y2 = y + (int)milkTea.get(milkTea, i);
+                y2 = y + (int)Array.get(milkTea, i);
             } else {
-                System.out.println("Nothing");
+                y2 = y;
+                x2 = x + (int)Array.get(milkTea, i);
             }
 
             System.out.println(x);
@@ -42,23 +53,48 @@ public class Boba extends Battleship {
         System.out.println("Where do you want to place your ships?\n" +
                 "Insert your starting x point.\n" +
                 "Please list the smaller point first.");
-        x = Hello.nextInt();
+        try {
+            x = Hello.nextInt();
+        } catch (Exception e) {
+            System.out.println("Error: Not an integer.");
+        }
         System.out.println("Insert your starting y point.\n" +
                 "Please list the smaller point first.");
-        y = Hello.nextInt();
+        try {
+            y = Hello.nextInt();
+        } catch (Exception e) {
+            System.out.println("Error: Not an integer.");
+        }
         System.out.println("Insert your ending x point.");
-        x2 = Hello.nextInt();
+        try {
+            x2 = Hello.nextInt();
+        } catch (Exception e) {
+            System.out.println("Error: Not an integer.");
+        }
         System.out.println("Insert your ending y point.");
-        y2 = Hello.nextInt();
+        try {
+            y2 = Hello.nextInt();
+        } catch (Exception e) {
+            System.out.println("Error: Not an integer.");
+        }
 
 
     }
 
     public static void fire() {
         System.out.println("Where would you like to fire?\nInsert your x value:");
-        int bye1 = Hello.nextInt();
+        try {
+            int bye1 = Hello.nextInt();
+        } catch (Exception e) {
+            System.out.println("Error: Not an integer.");
+        }
+
         System.out.println("Insert your y value:");
-        int bye2 = Hello.nextInt();
+        try {
+            int bye2 = Hello.nextInt();
+        } catch (Exception e) {
+            System.out.println("Error: Not an integer.");
+        }
 
         if (bye1 < 1 || bye1 > 10) {
             System.out.println("Invalid entry.");
