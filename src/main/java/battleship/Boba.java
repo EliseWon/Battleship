7bb759d5d1f1c595e998e9a48de013828f1d8e96
dmc-account <>
2020-01-16 14:24:58 -0800

    More Stuffs

--- src/main/java/battleship/Boba.java
+++ src/main/java/battleship/Boba.java
@@ -27,15 +27,13 @@ public class Boba extends Battleship {
             int z = (int) Array.get(shipLengths, i);
             int[] randomizedShipCoordinates = makeShip(z);
 
-            makeShip(z);
             x = (int) Array.get(randomizedShipCoordinates, 0);
             y = (int) Array.get(randomizedShipCoordinates, 1);
             x2 = (int) Array.get(randomizedShipCoordinates, 2);
             y2 = (int) Array.get(randomizedShipCoordinates, 3);
-            int ds = 4;
 
             while (x2 > 9 || y2 > 9) {
-                // randomizedShipCoordinates = makeShip(z); add this so that it's not an infinite loop
+                randomizedShipCoordinates = makeShip(z);
                 x = (int) Array.get(randomizedShipCoordinates, 0);
                 y = (int) Array.get(randomizedShipCoordinates, 1);
                 x2 = (int) Array.get(randomizedShipCoordinates, 2);
@@ -45,12 +43,11 @@ public class Boba extends Battleship {
             for (int m = 0; m < theBoard.length; m++) {
                 for (int n = 0; n < theBoard[0].length; n++) {
                     while (theBoard[x][y] == 1) {
-                     //   randomizedShipCoordinates =  makeShip(z); same thing
+                        randomizedShipCoordinates =  makeShip(z);
                         x = (int) Array.get(randomizedShipCoordinates, 0);
                         y = (int) Array.get(randomizedShipCoordinates, 1);
                         x2 = (int) Array.get(randomizedShipCoordinates, 2);
                         y2 = (int) Array.get(randomizedShipCoordinates, 3);
-
                     }
                     for (int a = x; x < x2; x++) {
                         for (int b = y; y < y2; y++) {
@@ -115,18 +112,13 @@ public class Boba extends Battleship {
 
     void fire() {
         System.out.println("Where would you like to fire?\nEnter the x value:");
-        try {
             int fireX = Hello.nextInt();
-        } catch (Exception e) {
             System.out.println("Error: Not an integer.");
-        }
+
 
         System.out.println("Enter the y value:");
-        try {
             int fireY = Hello.nextInt();
-        } catch (Exception e) {
             System.out.println("Error: Not an integer.");
-        }
 
         if (fireX < 1 || fireX > 10) {
             System.out.println("Invalid entry.");
